c2ed0af84d60071e65e8773607b977277757b1cd
prasad217 <cr6473@srmist.edu.in>
2024-09-28 00:16:57 +0530

    Initial commit

---
+++ src/components/Contact.js
@@ -0,0 +1,30 @@
+import React from 'react'
+
+const Contact = () => {
+    return (
+        <>
+            <div className="container">
+                <h2 className="py-3 text-center">Contact</h2>
+                <h3>
+                    Start working with us today. To get added as a doctor please email your details to any of the given admin below :
+                </h3>
+                <ul style={{color : "blue"}}>
+                    <li>
+                        <h4> 
+                       sai@gmail.com                      
+                        </h4>
+                    </li>
+                    <li>
+                        <h4>
+                        saimahesh@gmail.com
+                        </h4>
+                    </li>
+                </ul>
+            
+                
+                </div>
+        </>
+    )
+}
+
+export default Contact
---
+++ src/components/UserLogin.js
@@ -0,0 +1,117 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+
+class UserLogin extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            email: '',
+            password: '',
+            message: null,
+        };
+
+        this.login = this.login.bind(this);
+        this.onChange = this.onChange.bind(this);
+    }
+
+    login = e => {
+        if (this.state.password === '') {
+            alert('Password cannot be null');
+            this.props.history.push('/userLogin');
+            return false;
+        }
+        e.preventDefault();
+
+        // Simulated user object for testing
+        let dummyResponse = {
+            userType: 'patient', // Change to 'doctor' or 'admin' for testing other roles
+            email: this.state.email,
+            name: 'Test User',
+        };
+
+        // Simulate a successful login
+        this.setState({ message: 'Login successful.' });
+        console.log(this.state.message);
+
+        // Determine the user type and redirect accordingly
+        if (dummyResponse.userType === 'patient') {
+            sessionStorage.setItem('patient', JSON.stringify(dummyResponse));
+            this.props.history.push('/patientDashboard');
+        } else if (dummyResponse.userType === 'doctor') {
+            sessionStorage.setItem('doctor', JSON.stringify(dummyResponse));
+            this.props.history.push('/doctorDashboard');
+        } else {
+            sessionStorage.setItem('admin', JSON.stringify(dummyResponse));
+            this.props.history.push('/adminDashboard');
+        }
+    };
+
+    validateEmail() {
+        let email = document.getElementById('email').value;
+        let emailRegex = /\S+@\S+\.\S+/;
+        if (emailRegex.test(email) === true || email === '') {
+            return true;
+        } else {
+            document.getElementById('emailVR').innerHTML = 'Email format should be abc@xyz.com';
+            return false;
+        }
+    }
+
+    removeAlert() {
+        document.getElementById('emailVR').innerHTML = '';
+    }
+
+    onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+    render() {
+        return (
+            <>
+                <h2 className="text-center mt-5 mb-4">User Login</h2>
+                <form className="container bg-dark pt-2" style={{ width: '30vw' }}>
+                    <div className="form-group">
+                        <input
+                            id="email"
+                            type="email"
+                            className="form-control text-center mt-3"
+                            placeholder="Email"
+                            name="email"
+                            value={this.state.email}
+                            onChange={this.onChange}
+                            onBlur={this.validateEmail}
+                            onFocus={this.removeAlert}
+                            required
+                        />
+                        <span style={{ color: 'red' }} id="emailVR"></span>
+                    </div>
+                    <div className="form-group my-3">
+                        <input
+                            type="password"
+                            className="form-control text-center"
+                            name="password"
+                            placeholder="Password"
+                            value={this.state.password}
+                            onChange={this.onChange}
+                            required
+                        />
+                    </div>
+                    <div className="row my-3">
+                        <div className="col-sm-7">
+                            <button className="btn btn-success text-uppercase mb-3 offset-8" onClick={this.login}>
+                                LOGIN
+                            </button>
+                        </div>
+                        <div className="col-sm-5 mt-3">
+                            <NavLink className="text-light" to="/email-for-forgot-password">
+                                <h6>Forgot Password?</h6>
+                            </NavLink>
+                        </div>
+                    </div>
+                </form>
+                <span id="span"></span>
+            </>
+        );
+    }
+}
+
+export default UserLogin;
